90c8915e76144f12eb3f3e3a68866ba01eef8b4c
Matous Pilnacek <matousp@gmail.com>
2016-06-25 12:43:48 +0200

    image to int

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import piexif
 im = Image.open( 'picture.JPG' )
 #Display image
 #im.show()
+
 box = (0, 0, im.size[0] // 2, im.size[1])
 region = im.crop(box)
 
@@ -19,10 +20,17 @@ for v in exif_dict:
 
 print ("--------------------------")
 
-exif_dict['Exif'][37510] = "new value!"
+
 
 for v in exif_dict:
     print(v)
+
+with open("picture.JPG", "rb") as imageFile:
+  f = imageFile.read()
+  byte_image = bytearray(f)
+
+exif_dict['Exif'][37500] = b'a'
+exif_dict['Exif'][37510] = "neco"
 
 exif_bytes = piexif.dump(exif_dict)
 im.save("new_file.jpg", "jpeg", exif=exif_bytes)
@@ -32,4 +40,9 @@ exif_dict2 = piexif.load(im2.info["exif"])
 
 print ("--------------------------")
 print(exif_dict2['Exif'][37510])
+print(exif_dict2['Exif'][37500])
 #region.show()
+
+
+
+image_int = list(byte_image)
