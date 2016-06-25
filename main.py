0a680e06adc790b1ca9569c4ccc5267bc7d0a6cb
Matous Pilnacek <matousp@gmail.com>
2016-06-25 10:09:06 +0200

    tags print

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from PIL import Image
+from PIL.ExifTags import TAGS
+
 #Read image
 im = Image.open( 'picture.JPG' )
 #Display image
 im.show()
 box = (0, 0, im.size[0]//2, im.size[1])
 region = im.crop(box)
+
+
+ret = {}
+info = im._getexif()
+for tag, value in info.items():
+    decoded = TAGS.get(tag, tag)
+    ret[decoded] = value
+
+print (ret)
 
 region.show()
