f50c631794cad6aaac283c350ceba892b0549735
Matous Pilnacek <matousp@gmail.com>
2016-06-25 21:46:37 +0200

    docstrings

--- main.py
+++ main.py
@@ -7,13 +7,24 @@ import sys
 
 def split_image(im):
     """
-    Funkce split_imaVrací levou polovinu vstupního obrázku.
+    Funkce split_image crací levou polovinu vstupního obrázku im.
     """
+
     box = (0, 0, im.size[0] // 2, im.size[1])
     return im.crop(box)
 
 
 def add_metadata(im):
+    """
+    Funkce add_metadata vrací exif vstupniho obrazku im obohaceného o
+    textová a obrazová metadata v bytové podobě. Na uživatelském vstupu
+    jsou očekávány text a cesta k obrázku k připojení. Pokud cesta neexistuje
+    neboje obrazek příliš velký, je uživatel vyzván k opětovnému zadání.
+
+    Textová data jsou ukládána do exifu pod klíčem 37510 a obrazová
+    pod kódem 37500.
+    """
+
     meta_text = input('Zadejte textová metadata '
                       '(pokud nechcete pridavat, ponechte prazdne): ')
     if meta_text == "":
@@ -51,6 +62,11 @@ def add_metadata(im):
 
 
 def retrieve_metadata(im2):
+    """
+    Funkce retrieve_metadata nacte z obrazku v argumentu im2
+    nestandardni textova a obrazova metadata a zobrazi je.
+    """
+
     exif_dict2 = piexif.load(im2.info["exif"])
     print("Textová metadata:",
           exif_dict2['Exif'][37510][0:].decode("utf-8"))
@@ -61,6 +77,11 @@ def retrieve_metadata(im2):
 
 
 def image_loading(path):
+    """
+    Funkce retrieve_metadata nacte z obrazku v argumentu im2
+    nestandardni textova a obrazova metadata a zobrazi je.
+    """
+
     while True:
         try:
             im = Image.open(path)
@@ -74,6 +95,13 @@ def image_loading(path):
 
 
 def image_saving(path, exif_bytes, im2):
+    """
+    Funkce image_saving připojí exif v bytove podobě (exif_bytes)
+    k obrazku (im2) a ulozi ho na zadanou cestu (path). Pokud
+    pri ukladani dojde k vyjimce IOError (například soubor existuje
+    a nelze přepsat) je uživatel vyzván k novému vstupu.
+    """
+
     while True:
         try:
             im2.save(path, "jpeg", exif=exif_bytes)
@@ -91,6 +119,13 @@ def image_saving(path, exif_bytes, im2):
 
 
 def metadata_handling():
+    """
+    Funkce metadata_handling je hlavní funkcí skriptu. Uživatel
+    si na začátku zvolí, zda chce editovat obrázek (vyříznou polovinu)
+    a připojovat metadata nebo metadata neopak načítat. Následně je
+    vyzván k zadání dalších informací, na jejichž základě je řízen
+    běh programu.
+    """
     option = input('Chcete obrazek editovat (e) nebo'
                    ' nacist metadata (n)? ')
     while (option != "e"
