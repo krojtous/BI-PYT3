2e6366a9c10f6f3e116ee5a3dbbc8139409aa5fe
Matous Pilnacek <matousp@gmail.com>
2016-06-25 17:36:26 +0200

    metadata adding - input control

--- main.py
+++ main.py
@@ -7,55 +7,70 @@ def split_image(im):
     box = (0, 0, im.size[0] // 2, im.size[1])
     return im.crop(box)
 
-#path = input('Zadej cestu k stereoskopickemu obrazku: ')
-path = 'pictre.JPG'
 
+def add_metadata(im):
+    # meta_text = input('Zadejte textová metadata (pokud nechcete pridavat, ponechte prazdne): ')
+    meta_text = 'Toto je zkusebni meta text.'
+    exif_dict = piexif.load(im.info["exif"])
+    exif_dict['Exif'][37510] = meta_text
 
-#Read image
-im = Image.open( path )
-#Display image
-im.show()
-im2 = split_image(im)
-im2 = split_image(im)
+    # path = input('Zadejte cestu k obrazovym metadatum (ne vetsi nez 10 kB;pokud nechcete pridavat, ponechte prazdne): ')
+    with open("small.JPG", "rb") as imageFile:
+        f = imageFile.read()
+        byte_image = bytearray(f)
 
-#add_metadata()
-#save_image()
+    exif_dict['Exif'][37500] = byte_image
 
-
-
-
-
-exif_dict = piexif.load(im.info["exif"])
-for v in exif_dict:
-     print (v)
-
-
-print ("--------------------------")
+    exif_bytes = piexif.dump(exif_dict)
+    return exif_bytes
 
 
 
-for v in exif_dict:
-    print(v)
+def retrieve_metadata(im2):
+    exif_dict2 = piexif.load(im2.info["exif"])
+    print("Textová metadata: ", exif_dict2['Exif'][37510][0:])
 
-with open("small.JPG", "rb") as imageFile:
-  f = imageFile.read()
-  byte_image = bytearray(f)
+    retrieved_image = Image.open(io.BytesIO(exif_dict2['Exif'][37500]))
+    retrieved_image.show()
 
 
-exif_dict['Exif'][37500] = byte_image
-exif_dict['Exif'][37510] = "neco2"
+option = input('Chcete obrazek editovat (e) nebo nacist metadata (n)? ')
+while option != "e" and option != "n":
+    option = input('Napiste "n" nebo "e":')
 
-exif_bytes = piexif.dump(exif_dict)
-im.save("new_file.jpg", "jpeg", exif=exif_bytes)
+if option == "e":
+    #path = input('Zadej cestu k stereoskopickemu obrazku: ')
+    path = 'picture2.JPG'
+    while True:
+        try:
+            im = Image.open( path ) #Read image
+        except FileNotFoundError:
+            print("Na zadane ceste:",path,"neexistuje obrazek. Zadejte novou cestu:")
+            path = input()
+        else:
+            print("Obrazek:", path, " nacten.")
+            break
+    im2 = split_image(im)
+    exif_bytes = add_metadata(im)
+    im2.save("new_file.jpg", "jpeg", exif=exif_bytes)
+else:
+    path2 = "new_file.jpg"
+    im = Image.open(path2)
+    im.show()
+    retrieve_metadata(im)
 
-im2 = Image.open( 'new_file.jpg' )
-exif_dict2 = piexif.load(im2.info["exif"])
 
-print ("--------------------------")
-print(exif_dict2['Exif'][37510])
-print(exif_dict2['Exif'][37500])
 
-retrieved_image = Image.open(io.BytesIO(exif_dict2['Exif'][37500]))
-retrieved_image.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
