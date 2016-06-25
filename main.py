3ddb09fb607789c203a72a237db19046b9627c02
Matous Pilnacek <matousp@gmail.com>
2016-06-25 11:58:31 +0200

    add text meta

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+import piexif
 
 #Read image
 im = Image.open( 'picture.JPG' )
 #Display image
-im.show()
-box = (0, 0, im.size[0]//2, im.size[1])
+#im.show()
+box = (0, 0, im.size[0] // 2, im.size[1])
 region = im.crop(box)
 
 
-ret = {}
-info = im._getexif()
-for tag, value in info.items():
-    decoded = TAGS.get(tag, tag)
-    ret[decoded] = value
+w, h = im.size
 
-print (ret)
+exif_dict = piexif.load(im.info["exif"])
+for v in exif_dict:
+     print (v)
 
-region.show()
+
+print ("--------------------------")
+
+exif_dict["meta_text"] = "new value"
+
+for v in exif_dict:
+    print(v)
+    #for k in exif_dict[v]:
+     #   print(k, ':', exif_dict[v][k])
+
+
+exif_bytes = piexif.dump(exif_dict)
+im.save("new_file.jpg", "jpeg", exif=exif_bytes)
+
+#region.show()
